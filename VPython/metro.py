a6278a232f4c5b2c1cdf962cc97d608d7a059d7b
Danko Denchuk Hapchenko <61185188+Danko-Denchuk@users.noreply.github.com>
2022-01-21 18:46:05 +0100

    Add files via upload

--- VPython/metro.py
+++ VPython/metro.py
@@ -1,8 +1,4 @@
 from vpython import *
-from scipy import interpolate
-import time
-import numpy as np
-import random
 
 scene.width = 1600
 scene.height = 900
@@ -28,6 +24,7 @@ class station():
         self.b = b
         self.c = c
 
+
 class Locomotive():
     def __init__(self, x0, y0, z0, L, H, W, a, b, c):
         self.x0 = x0
@@ -39,6 +36,7 @@ class Locomotive():
         self.a = a
         self.b = b
         self.c = c
+
 
 class Wagon():
     def __init__(self, x0, y0, z0, L, H, W, a, b, c):
@@ -52,21 +50,10 @@ class Wagon():
         self.b = b
         self.c = c
 
-# class rails():
-#     def __init__(self, x0, y0, z0, a, b, c):
-#         self.x0 = x0
-#         self.y0 = y0
-#         self.z0 = z0
-#         self.a = a
-#         self.b = b
-#         self.c = c
 
 axis_x=arrow(pos=vec(0,0,0), axis=vec(10,0,0), color=color.red, shaftwidth=1)
 axis_y=arrow(pos=vec(0,0,0), axis=vec(0,10,0), color=color.green, shaftwidth=1)
 axis_z=arrow(pos=vec(0,0,0), axis=vec(0,0,10), color=color.blue, shaftwidth=1)
-
-
-
 
 class Stretch():
     def __init__(self, x0, y0, z0, x1, y1, z1, x2, y2, z2, x3, y3, z3):
@@ -86,36 +73,17 @@ class Stretch():
         self.y3=y3
         self.z3=z3
 
+
 ruta=list()
-via1=list()
-via2=list()
-
-ruta.append(Stretch(0, 0, 0,
-                     30, 0, 30,
-                     60, 0, 60,
-                     120, 35, 90))
-
-via1.append(Stretch(1, 0, 0,
-                     60, -20, 30,
-                     30, 20, 90,
-                     121, 0, 121))
-
-via2.append(Stretch(-1, 0, -1,
-                     60, -20, 30,
-                     30, 20, 90,
-                     119, 0, 119))
-
-
-# tramo.append(Stretch(90, 0, 90,
-#                      120, 0, 120,
-#                      150, 0, 150,
-#                      200, 25, 200))
+ruta2=list()
+ruta.append(Stretch(0, 0, 0, 30, 0, 30, 60, 0, 60, 120, 35, 90))
+ruta2.append(Stretch(0, 0, 0, 30, 0, 30, 60, 0, 60, 240, 35, -190))
 
 def calc_tramo(tramo, visible):
     path = curve(color=color.cyan, radius=0.1, visible=visible)
     aux_path = curve(color=color.red, radius=0.1, visible=0)
 
-    for n in range(0, len(tramo)):
+    for n in range(len(tramo)):
         x0=tramo[n].x0
         y0=tramo[n].y0
         z0=tramo[n].z0
@@ -135,13 +103,9 @@ def calc_tramo(tramo, visible):
         for t in range(0,10000):
             t=t/10000
             aux_path.append(vec(x0, y0, z0)*(1-t)**3+vec(x1, y1, z1)*(3*t*(1-t)**2)+vec(x2, y2, z2)*(3*(t**2)*(1-t))+vec(x3, y3, z3)*(t**3))
-    # for t in range(-1000,1000,1):
-    #     aux_path.append(vec(t/10, 5*np.sin(t/100)-2.5*np.cos(t/100), 10*np.cos(t/100)))
-
 
     dt = 1
     integral=0
-    marker = list()
 
     for n in range(aux_path.npoints-1):
         if n > 1:
@@ -149,129 +113,84 @@ def calc_tramo(tramo, visible):
         if integral >= dt:
             integral = 0
             path.append(aux_path.point(n)['pos'])
-            # marker.append(sphere(pos=aux_path.point(n)['pos'], radius = 2, color = color.red))
+
+    track1=curve(color=color.cyan, radius=0.1)
+    track2=curve(color=color.cyan, radius=0.1)
+
+    for n in range(path.npoints-2):
+        normal=norm(cross(path.point(n+1)['pos']-path.point(n)['pos'],vec(0,1,0)))
+
+        track1.append(vec(path.point(n)['pos']+0.33*normal))
+        track2.append(vec(path.point(n)['pos']-0.33*normal))
+
     return path
 
+
 path=calc_tramo(ruta, 0)
-# track1=calc_tramo(via1, 0)
-# track2=calc_tramo(via2, 1)
-
-track1=curve(color=color.cyan, radius=0.1)
-track2=curve(color=color.cyan, radius=0.1)
-
-for n in range(path.npoints-2):
-    normal=norm(cross(path.point(n+1)['pos']-path.point(n)['pos'],vec(0,1,0)))
-
-    track1.append(vec(path.point(n)['pos']+0.33*normal))
-    track2.append(vec(path.point(n)['pos']-0.33*normal))
-
-print("Number of points in path = " + str(path.npoints))
-
-# Decorative stars
-stars=list()
-for star in range(1,1000):
-    stars.append(sphere(pos=vec(random.randrange(-1000, 1000), random.randrange(-1000, 1000), random.randrange(-1000, 1000)), radius=1, color=color.white))
-
-# extreme1 = sphere(pos=path.point(0), radius=1, color=color.cyan)
-# extreme2 = sphere(pos=path.point(path.npoints-1), radius=1, color=color.cyan)
-
-# for x in range(5, 10):
-#     sol = 3*x
-#     path.append(vec(x, 0, -sol))
-
-# links = curve(path[0], path[len(path-1)])
+path2=calc_tramo(ruta2,0)
 
 locomotive = Locomotive(0, 1, 0, 5, 1, 1, 0, 0, -1)
-wagon = Wagon(0, 1, -200, 5, 1, 1, 0, 0, -1)
 
-BoxLocomotive=box(pos=vec(locomotive.x0, locomotive.y0, locomotive.z0), color=color.orange, length=locomotive.L, height=locomotive.H, width=locomotive.W, axis=vec(locomotive.a, locomotive.b, locomotive.c))
-BoxWagon1=box(pos=vec(wagon.x0, wagon.y0, wagon.z0), color=color.orange, length=wagon.L, height=wagon.H, width=wagon.W, axis=vec(wagon.a, wagon.b, wagon.c))
-BoxWagon2=box(pos=vec(wagon.x0, wagon.y0, wagon.z0), color=color.orange, length=wagon.L, height=wagon.H, width=wagon.W, axis=vec(wagon.a, wagon.b, wagon.c))
-BoxWagon3=box(pos=vec(wagon.x0, wagon.y0, wagon.z0), color=color.orange, length=wagon.L, height=wagon.H, width=wagon.W, axis=vec(wagon.a, wagon.b, wagon.c))
+def make_train(cabs, line):
+    locomotive = Locomotive(0, 1, 0, 5, 1, 1, 0, 0, -1)
+    train=list()
+    if line==1:
+        col=color.cyan
+    if line==2:
+        col=color.orange
 
-speed_list=list()
-acceleration_list=list()
+    for n in range(cabs):
+        train.append(box(color=col, length=locomotive.L, height=locomotive.H, width=locomotive.W, axis=vec(locomotive.a, locomotive.b, locomotive.c)))
+    return train
+
+BoxLocomotive=make_train(6,1)
+AnotherLocomotive=make_train(6,2)
 
 is_followed=False
 def follow(r):
     global is_followed
     if r.checked:
-        scene.camera.follow(BoxLocomotive)
+        scene.camera.follow(BoxLocomotive[0])
         scene.range=10
     else:
         scene.camera.follow(None)
 
-is_deck=False
-def deck_view(r):
-    global is_deck
-    if r.checked:
-        is_deck=True
-    else:
-        is_deck=False
 
-checkbox(bind=follow, text='Follow train') # text to right of checkbox
+checkbox(bind=follow, text='Follow train')
 scene.append_to_caption('\t\t')
 
-checkbox(bind=deck_view, text='Deck view') # text to right of checkbox
-scene.append_to_caption('\n\n')
+def calc_speed(path):
+    speed=list()
+    for s in range(path.npoints-1):
+        speed.append(norm(path.point(s+1)['pos']-path.point(s)['pos']))
+    return speed
 
-velocity=label(yoffset=100, box=False, color=color.orange)
-passengers=label(yoffset=20, box=False, color=color.orange)
 
-while True:
-    for n in range(path.npoints-2):
-        if running:
-            speed=norm(path.point(n+1)['pos']-path.point(n)['pos'])
-            velocity.text=(str(round(mag(speed), 2))+'<i>m/s<i>')
-            velocity.pos=BoxLocomotive.pos
-            # speed2=norm(path.point(n+2)['pos']-path.point(n+1)['pos'])
-            # accel=speed2-speed
-            BoxLocomotive.up=norm(cross(speed, vec(0,1,0)))
+def move_train(train, path, n, speed, mode):
+    if path.npoints-n > 5:
+        for t in range(train-1):
+            if n-5*t > 0:
+                mode[t].up=norm(cross(speed[n-5*t], vec(0,1,0)))
+                mode[t].axis=5*speed[n-5*t]
+                mode[t].pos.x=path.point(n-5*t)['pos'].x
+                mode[t].pos.y=path.point(n-5*t)['pos'].y+1
+                mode[t].pos.z=path.point(n-5*t)['pos'].z
+    else:
+        return 0
 
-            speed_list.append(speed)
-            BoxLocomotive.axis=5*norm(speed)
-            BoxLocomotive.pos.x=path.point(n)['pos'].x
-            BoxLocomotive.pos.y=path.point(n)['pos'].y+1
-            BoxLocomotive.pos.z=path.point(n)['pos'].z
+t1=clock()*1000
+vel=calc_speed(path)
+vel2=calc_speed(path2)
+n=0
+while running:
+    if running:
+        if ((clock()*1000)-t1)>12:
+            line1=move_train(len(BoxLocomotive), path, n, vel, BoxLocomotive)
+            line2=move_train(len(AnotherLocomotive), path2,  n, vel2, AnotherLocomotive)
+            t1=clock()*1000
+            n=n+1
+            if line1==0 and line2==0:
+                n=0
 
-            if n > 5:
-                BoxWagon1.axis=5*speed_list[n-5]
-                BoxWagon1.up=norm(cross(speed_list[n-5],vec(0,1,0)))
-                BoxWagon1.pos.x=path.point(n-5)['pos'].x
-                BoxWagon1.pos.y=path.point(n-5)['pos'].y+1
-                BoxWagon1.pos.z=path.point(n-5)['pos'].z
-                passengers.text=(str('<i>15 passengers<i>'))
-                passengers.pos=BoxWagon1.pos
-
-            if n > 10:
-                BoxWagon2.axis=5*speed_list[n-10]
-                BoxWagon2.up=norm(cross(speed_list[n-10],vec(0,1,0)))
-                BoxWagon2.pos.x=path.point(n-10)['pos'].x
-                BoxWagon2.pos.y=path.point(n-10)['pos'].y+1
-                BoxWagon2.pos.z=path.point(n-10)['pos'].z
-
-            if n > 10:
-                BoxWagon3.axis=5*speed_list[n-15]
-                BoxWagon3.up=norm(cross(speed_list[n-15],vec(0,1,0)))
-                BoxWagon3.pos.x=path.point(n-15)['pos'].x
-                BoxWagon3.pos.y=path.point(n-15)['pos'].y+1
-                BoxWagon3.pos.z=path.point(n-15)['pos'].z
-
-            # scene.camera.pos.x=BoxLocomotive.pos.x
-            # scene.camera.pos.y=BoxLocomotive.pos.y+1
-            # scene.camera.pos.z=BoxLocomotive.pos.z
-            if is_deck:
-                # scene.camera.pos.x=BoxLocomotive.pos.x
-                # scene.camera.pos.y=BoxLocomotive.pos.y+1
-                # scene.camera.pos.z=BoxLocomotive.pos.z
-                scene.center.x=BoxLocomotive.pos.x-5*speed.x
-                scene.center.y=BoxLocomotive.pos.y+3
-                scene.center.z=BoxLocomotive.pos.z-5*speed.z
-                scene.camera.axis=speed
-
-            rate(30)
-        else:
-            break
-    speed_list=[]
-    acceleration_list=[]
-# BoxLocomotive.up=vec(0,1,0)
+    else:
+        break
